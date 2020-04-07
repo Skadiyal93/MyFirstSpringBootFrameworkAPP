cb78d6d8d0aeb7a554fb0ad028daa60cb58af426
Satish <Satishblockchain@gmail.com>
2020-04-06 20:17:31 -0400

    MY First Commit

---
+++ Satish_firstspring/src/test/java/com/Satish/Satish_firstspring/SatishFirstspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Satish.Satish_firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatishFirstspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
